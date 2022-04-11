b8d3be64b9cb8a21d943d1595267ce4d692df111
Sanjin888 <sanjinkurtalija@gmail.com>
2022-04-12 00:00:51 +0200

    Header skeleton

--- views/header.php
+++ views/header.php
@@ -8,6 +8,30 @@
     <link rel="stylesheet" href="./css/header.css">
     <title>Document</title>
 </head>
+<header>
+    <!-- navbar -->
+ 
+    <nav class="navbar navbar-expand-lg navbar-light bg-light ">
+        <div class="container">
+            <a class="navbar-brand" href="#">Sea</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link" href="#">Explore</a>
+                    <a class="nav-link" href="#">My Profile</a>
+                </div> 
+            </div>      
+            <div class="text-right" >
+  	            <button id="nav-button" type="button" class="btn btn-primary">Dugme</button>
+            </div>
+        </div>
+    </nav>
+
+
+</header>
 <body>
     
 </body>
